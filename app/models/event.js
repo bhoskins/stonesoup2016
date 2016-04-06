491fd6600c3e92e61a7622b8f3de0d55ad2998a0
Beth Hoskins <bhoskins@charter.net>
2016-04-06 00:22:24 -0400

    basic routes and models

---
+++ app/models/event.js
@@ -0,0 +1,16 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  // id: DS.attr('number'),
+  // title: DS.attr('string'),
+  // day: DS.attr('string'),
+  // time: DS.attr('string'),
+  // description: DS.attr('string'),
+  // icons: DS.hasMany('icons'),
+  // storytellers: DS.hasMany('storyteller'),
+  // schedule: DS.belongsTo('schedule'),
+  // dateTime: Ember.computed('day', 'time', function() {
+  //   return ${this.get('day')} ${this.get('time')}
+  // })
+
+});
---
+++ app/routes/tellers.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return $.getJSON("assets/js/data.json").then(function(data) {
+      return data.tellers;
+    });
+  }
+});
