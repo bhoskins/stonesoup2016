5b1f9dd1fc4284712b1dd7e3a68e55bfd7c8c831
Beth Hoskins <bhoskins@charter.net>
2016-04-06 13:07:38 -0400

    basic routes

--- app/router.js
+++ app/router.js
@@ -6,13 +6,6 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('index', {path: '/'});
-  this.route('about', {path: '/about'});
-  this.route('events', {path: '/events'});
-  this.route('event', {path: '/events/:event_id'});
-  this.route('tellers', {path: '/tellers'});
-  this.route('map', {path: '/map'});
-  this.route('contact', {path: '/contact'});
 });
 
 export default Router;
--- app/routes/tellers.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model: function() {
-    return $.getJSON("assets/js/data.json").then(function(data) {
-      return data.tellers;
-    });
-  }
-});
