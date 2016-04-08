84835a4301bf71de44047bb5b0aea3adeea7878e
Beth Hoskins <bhoskins@charter.net>
2016-04-07 20:58:24 -0400

    map markers added

--- app/routes/map.js
+++ app/routes/map.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return $.getJSON("assets/js/data.json").then(function(data) {
+      return data.markers;
+    });
+  },
   actions: {
     markerClick: function() {
       alert('You clicked a marker');
