eb0de9f8dc4e353842f34125d7ae8846c2e6eb0f
Beth Hoskins <bhoskins@charter.net>
2016-04-10 20:17:54 -0400

    Contact Page styled

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    padBody: function(){
+      Ember.$('.navbar-toggle').on('click', function(){
+
+        //Ember.$('.container-all').css('padding-top', '216px');
+       });
+    }
+  }
+
+
 });
